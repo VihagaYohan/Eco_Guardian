9331b13ae342723a9c077c8853d3e370f7a5fbad
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-12-20 16:32:02 +0530

    code changes

--- app/pages/auth/LoginScreen.js
+++ app/pages/auth/LoginScreen.js
@@ -1,10 +1,25 @@
-import React, { Component, useLayoutEffect } from "react";
-import { StyleSheet, SafeAreaView, View, Text, Dimensions } from "react-native";
+import React, { Component, useLayoutEffect, useRef } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 // components
-import { UITextView, UIContainer, UIFAB } from "../../components";
+import { UITextView, UIContainer, UIFAB, UIButton } from "../../components";
+
+// constants
+import { COLORS, CONSTANTS, DIMENSION } from "../../constants";
 
 const LoginScreen = ({ navigation, route }) => {
+  const scrollRef = useRef();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "Logs",
@@ -14,15 +29,96 @@ const LoginScreen = ({ navigation, route }) => {
 
   return (
     <UIContainer>
-      <View style={{ flex: 1 }}>
-        <UITextView text="Login screen page" textStyle={{ color: "red" }} />
+      <ScrollView
+        style={{ flex: 1, marginVertical: DIMENSION.MARGIN }}
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            borderWidth: 1,
+          }}
+        ></View>
 
-        <UIFAB onPress={() => console.log("onpress")} />
-      </View>
+        <View style={{ marginVertical: DIMENSION.MARGIN * 4 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              paddingHorizontal: DIMENSION.PADDING,
+              justifyContent: "space-between",
+            }}
+          >
+            <UIButton
+              label="Eco Guardian"
+              onPress={() =>
+                scrollRef.current.scrollTo({
+                  x: DIMENSION.SCREEN_WIDTH,
+                  y: 0,
+                  animated: true,
+                })
+              }
+              buttonStyle={{
+                backgroundColor: COLORS.primaryColor,
+                paddingVertical: 10,
+                width: (DIMENSION.SCREEN_WIDTH - DIMENSION.PADDING * 2) / 2,
+                borderRadius: 0,
+              }}
+            />
+
+            <UIButton
+              label="Eco Officer"
+              onPress={() =>
+                scrollRef.current.scrollTo({
+                  x: -(DIMENSION.SCREEN_WIDTH - DIMENSION.PADDING * 2) / 2,
+                  y: 0,
+                  animated: true,
+                })
+              }
+              buttonStyle={{
+                backgroundColor: "rgba(102,187,106, 0.8)",
+                paddingVertical: 10,
+                width: (DIMENSION.SCREEN_WIDTH - DIMENSION.PADDING * 2) / 2,
+                borderRadius: 0,
+              }}
+            />
+          </View>
+
+          <ScrollView
+            ref={scrollRef}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+            style={{
+              paddingVertical: DIMENSION.PADDING * 2,
+              width: DIMENSION.SCREEN_WIDTH - DIMENSION.PADDING * 2,
+              marginHorizontal: DIMENSION.PADDING * 2,
+            }}
+          >
+            <View style={styles.formholder}>
+              <UITextView text="login" />
+            </View>
+
+            <View style={styles.formholder}>
+              <UITextView text="register" />
+            </View>
+          </ScrollView>
+        </View>
+      </ScrollView>
     </UIContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  formholder: {
+    borderWidth: 1,
+    width: DIMENSION.SCREEN_WIDTH - DIMENSION.PADDING * 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default LoginScreen;
